5a451776fb51768cf709f7925c445412de52636b
Giulio Scattolin <giulio.scattolin@gmail.com>
2024-02-06 18:27:23 +0100

    enable short URLs
    See https://www.mediawiki.org/wiki/Manual:Short_URL/Nginx

--- mediawiki-html/entrypoint.sh
+++ mediawiki-html/entrypoint.sh
@@ -7,15 +7,15 @@ main () {
 }
 
 make_mediawiki_directory () {
-    mkdir -p /var/www/${MEDIAWIKI_HOST}/html
+    mkdir -p /var/www/${MEDIAWIKI_HOST}/html/w
 }
 
 install_mediawiki () {
     if ! cat /var/www/${MEDIAWIKI_HOST}/URL | grep -q ${MEDIAWIKI_TARBALL_URL}; then
-        rm -rf /var/www/${MEDIAWIKI_HOST}/html/*
+        rm -rf /var/www/${MEDIAWIKI_HOST}/html/w/*
         wget ${MEDIAWIKI_TARBALL_URL}
         tar -xvf mediawiki-*.tar.gz
-        mv mediawiki-*/* /var/www/${MEDIAWIKI_HOST}/html
+        mv mediawiki-*/* /var/www/${MEDIAWIKI_HOST}/html/w
         rm -r mediawiki-*
         echo ${MEDIAWIKI_TARBALL_URL} > /var/www/${MEDIAWIKI_HOST}/URL
     fi
@@ -32,7 +32,7 @@ make_nginx_own_mediawiki () {
     # uid=101(nginx) gid=101(nginx) groups=101(nginx)
     #
     # Source: https://hub.docker.com/_/nginx
-    chown -R 101:101 /var/www/${MEDIAWIKI_HOST}/html
+    chown -R 101:101 /var/www/${MEDIAWIKI_HOST}/html/w
 }
 
 main
